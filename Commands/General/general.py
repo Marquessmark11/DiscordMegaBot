50aa0288de465920ffaa3c10f1bdb66d5d912990
Con <50535379+ConnorTippets@users.noreply.github.com>
2020-12-24 03:52:09 -0600

    Add source command

--- Commands/General/general.py
+++ Commands/General/general.py
@@ -193,6 +193,40 @@ class General(commands.Cog):
     @commands.command()
     async def allCommands(self, ctx):
         await ctx.send(len(self.bot.commands))
+    
+    @commands.command()
+    async def source(self, ctx, *, command: str = None):
+        source_url = 'https://github.com/ConnorTippets/DiscordMegaBot'
+        branch = 'main'
+        if command is None:
+            return await ctx.send(source_url)
+
+        if command == 'help':
+            src = type(self.bot.help_command)
+            module = src.__module__
+            filename = inspect.getsourcefile(src)
+        else:
+            obj = self.bot.get_command(command.replace('.', ' '))
+            if obj is None:
+                return await ctx.send('Could not find command.')
+
+            # since we found the command we're looking for, presumably anyway, let's
+            # try to access the code itself
+            src = obj.callback.__code__
+            module = obj.callback.__module__
+            filename = src.co_filename
+
+        lines, firstlineno = inspect.getsourcelines(src)
+        if not module.startswith('discord'):
+            # not a built-in command
+            location = os.path.relpath(filename).replace('\\', '/')
+        else:
+            location = module.replace('.', '/') + '.py'
+            source_url = 'https://github.com/Rapptz/discord.py'
+            branch = 'master'
+
+        final_url = f'<{source_url}/blob/{branch}/{location}#L{firstlineno}-L{firstlineno + len(lines) - 1}>'
+        await ctx.send(final_url)
 
 def setup(bot):
     bot.add_cog(General(bot))
