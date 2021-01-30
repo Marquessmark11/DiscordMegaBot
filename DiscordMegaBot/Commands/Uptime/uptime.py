6cb5ab34fd00d8ec9746de8c9197a2dcaa9ee2fa
Con <50535379+ConnorTippets@users.noreply.github.com>
2021-01-30 00:18:13 -0600

    Bulk update files (again)

--- DiscordMegaBot/Commands/Uptime/uptime.py
+++ DiscordMegaBot/Commands/Uptime/uptime.py
@@ -7,18 +7,10 @@ class Uptime(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
-    @commands.command()
+    @commands.command(brief='Shows how long the bot has been on (in human time)')
     async def uptime(self, ctx):
-        #end = time.time()
-        #difference = end - start
-        #self.bot.uptime_seconds = difference
-        #delta = datetime.timedelta(seconds = difference)
-        #self.bot.uptime_delta = delta
-        #humandelta = humanize.naturaldelta(delta)
-        #self.bot.humanized_uptime_delta = humandelta
         embed = discord.Embed(title='Uptime', color = random.randint(100000, 999999), description=self.bot.uptime_delta_humanized())
         await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Uptime(bot))
-    
