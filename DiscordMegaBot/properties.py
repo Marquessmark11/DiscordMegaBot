344d34b964802a9158b4088ad2c120b901120882
Con <50535379+ConnorTippets@users.noreply.github.com>
2021-02-07 03:17:11 -0600

    Delete properties.py

--- DiscordMegaBot/properties.py
+++
@@ -1,9 +0,0 @@
-import discord as api
-prefix = ':'
-token = '<your token>'
-dagpi_token = '<your dagpi token>'
-intents = api.Intents.default()
-intents.members = True
-intents.presences = True
-insensitiveCase = True
-ownerID = "<your id> (without quotes)"
